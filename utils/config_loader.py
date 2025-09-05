c09cae6847ac8323113a3a85ab72eab0b85adbe0
arshad abbas <arshadblouch81@gmail.com>
2025-09-05 13:44:52 +0500

    Resolve config loader issue

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,12 +1,12 @@
 import yaml
 from pathlib import Path
 
-def load_config_old(config_path: str = "config\config.yaml") -> dict:
+def load_config_old(config_path: str = "config/config.yaml") -> dict:
     with open(config_path, "r") as file:
         config=yaml.safe_load(file)
     return config
 
-def load_config(config_path: str = "config\config.yaml") -> dict:
+def load_config(config_path: str = "config/config.yaml") -> dict:
     # Get the path relative to this script
     config_path = Path(__file__).resolve().parent.parent / 'config' / 'config.yaml'
 
